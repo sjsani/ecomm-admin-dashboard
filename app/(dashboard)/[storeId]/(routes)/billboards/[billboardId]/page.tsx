1b0212eb83eb626aa74f8f19aa210bb942069bb2
sjsani <sjsani.official@gmail.com>
2025-10-27 21:44:32 +0500

    last fix

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,25 +1,23 @@
-import { BillboardForm } from './components/BillboardForm';
 import prismadb from '@/lib/prismadb';
-import React from 'react';
+import React from 'react'
+import { BillboardForm } from './components/BillboardForm';
 
-interface BillboardPageProps {
-  params: {
-    billboardId: string;
-  };
-}
+const BillboardPage = async ({
+  params}:
+{params:{billboardId:string}}) => {
 
-const BillboardPage = async ({ params }: BillboardPageProps) => {
-  const billboard = await prismadb.billboard.findUnique({
-    where: { id: params.billboardId },
-  });
-
+  const billboard= await prismadb.billboard.findUnique({
+    where:{
+      id:params.billboardId
+    }
+  })
   return (
-    <div className="flex-col">
-      <div className="flex-1 space-y-4 p-8 pt-6">
+    <div className='flex-col'>
+      <div className='flex-1 space-y-4 p-8 pt-6'>
         <BillboardForm initialData={billboard} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default BillboardPage as any;
+export default BillboardPage;
